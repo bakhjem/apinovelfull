add5d76f808f7701215246c8578099323ec2abf1
khiêm bá <bakhjem@gmail.com>
2021-01-16 15:00:10 +0700

    add

---
+++ routes/novel.js
@@ -0,0 +1,108 @@
+var express = require("express");
+var router = express.Router();
+const request = require("request-promise");
+const cheerio = require("cheerio");
+var cookieParser = require("cookie-parser");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  var novel = [];
+  var idnovels = req.query.id;
+  const URL = "https://truyenfull.vn/";
+
+  const getPageContent = uri => {
+    const options = {
+      uri,
+      headers: {
+        "User-Agent": "Request-Promise"
+      },
+      transform: body => {
+        return cheerio.load(body);
+      }
+    };
+
+    return request(options);
+  };
+  var data = [];
+  var novelsname = null;
+  var author = null;
+  var genresdata = [];
+  var chapterlist = [];
+  var dateupdate = null;
+  var view = null;
+  var othername = null;
+  var lasterchapter = null;
+  var idnovel = null;
+  var idchapter = null;
+  var cover = null;
+  var des = null;
+  var lastupdates = [];
+  var update_time = null;
+  var id = null;
+  var totalpages = null;
+  getPageContent(URL + idnovels).then($ => {
+    novelsname = $("h3.title").text();
+    cover = $('.book img').attr('src');
+    // cover = 'https://webnovel.online'+cover;
+    // othername = $(".truyen_info_right li:nth-child(1) span").text();
+    author = $("[itemprop=author]").text();
+    console.log(author)
+    $("[itemprop=genre]").each(function (result) {
+      genres = $(this).text();
+      idgenres = $(this).attr("href");
+      idgenres = idgenres.slice(idgenres.search("the-loai") + 9);
+      //   console.log(idgenres);
+      genresdata.push({
+        genrename: genres,
+        idgenres: idgenres
+      });
+    });
+    status = $("span.text-primary").text();
+    description = $("[itemprop=description]").html();
+    // des = $("#noidungm").text();
+    // // description = description.slice(1,description.search('<hr>'))
+    $(".list-chapter li").each(function (result) {
+      $(this)
+        .find("a")
+        .each(function () {
+          chaptername = $(this).text();
+          var chapterid = $(this).attr("href");
+          idchapter = chapterid.slice(
+            chapterid.search(idnovels + "/") + (idnovels.length + 23)
+          );
+          chapterlist.unshift({
+            chaptername: chaptername,
+            idchapter: idchapter
+          })
+        });
+
+    });
+   let page = $("ul.pagination.pagination-sm li a").last().attr("href");
+   let lastpage = '';
+   if(page !== undefined){
+    lastpage  = page.slice(page.indexOf('trang-')+6,(page.indexOf('#list-chapter'))-1)
+   }
+   else {
+    lastpage = '1'
+   }
+   console.log(lastpage)
+    novel = {
+      novelsname: novelsname,
+      idnovels: idnovels,
+      // othername: othername,
+      author: author,
+      cover: cover,
+      genresdata: genresdata,
+      status: status,
+      // des: des,
+      description: description,
+      chapterlist: chapterlist,
+      totalpage: lastpage
+    }
+    // console.log(chapterlist);
+    res.send(JSON.stringify(novel));
+  });
+});
+
+module.exports = router;
---
+++ routes/search.js
@@ -0,0 +1,119 @@
+var express = require("express");
+var router = express.Router();
+const request = require("request-promise");
+const cheerio = require("cheerio");
+var cookieParser = require("cookie-parser");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  var novel = [];
+  var id = req.query.id;
+  var page = req.query.page;
+  const URL =
+    "https://truyenfull.vn/tim-kiem/?tukhoa=";
+
+  const getPageContent = uri => {
+    const options = {
+      uri,
+      headers: {
+        "User-Agent": "Request-Promise"
+      },
+      transform: body => {
+        return cheerio.load(body);
+      }
+    };
+
+    return request(options);
+  };
+  var data = [];
+  var novelsname = null;
+  var lasterchapter = null;
+  var idnovel = null;
+  var idchapter = null;
+  var cover = null;
+  var lastupdates = [];
+  var update_time = null;
+  var view = null;
+  var totalpages = null;
+  var des = null;
+  console.log(URL + id + '&page=' + page)
+  getPageContent(URL + id + '&page=' + page).then($ => {
+    // console.log(
+    //   "http://www.nettruyen.com/tim-truyen?status=-1&sort=15&page=" + page
+    // );
+    // var pagett = $(".pagination-outter ul li.hidden").text();
+    // totalpage = pagett.slice(pagett.search("/") + 2);
+    $(".list.list-truyen.col-xs-12 .row").each(function (result) {
+      // console.log($(this).html())
+      $(this)
+        .find(".truyen-title a")
+        .each(function () {
+          novelsname = $(this).text();
+          idnovel = $(this).attr('href');
+          // idnovel = idnovel.slice(1)
+          idnovel = idnovel.slice(idnovel.lastIndexOf(".vn/") + 4);
+
+        });
+      $(this)
+        .find(".col-xs-2.text-info a")
+        .each(function () {
+          lasterchapter = $(this).text();
+          var chapterid = $(this).attr('href');
+          idchapter = chapterid.slice(chapterid.lastIndexOf(idnovel) + (idnovel.length));
+
+        });
+      $(this)
+        .find(".author")
+        .each(function () {
+          view = $(this).text();
+        });
+
+      $(this)
+        .find(".lazyimg")
+        .each(function () {
+          cover = $(this).attr('data-desk-image');
+          // cover = 'https://webnovel.online' + cover;
+          // console.log(cover);
+          //   console.log(idchapter)
+        });
+      // $(this)
+      //   .find("span:nth-child(2)")
+      //   .each(function () {
+      //     view = $(this).text();
+      //     // console.log(cover);
+      //     //   console.log(idchapter)
+      //   });
+
+      data.push({
+        'novelsname': novelsname,
+        'idnovel': idnovel,
+        'lasterchapter': lasterchapter,
+        'idchapter': idchapter,
+        'cover': cover,
+        'author': view,
+        // 'lastupdates': lastupdates
+      })
+    });
+    var totalpage = $("ul.pagination.pagination-sm li a").eq(-2).attr("href");
+    console.log(totalpage)
+    let lastpage = '';
+    if (totalpage !== undefined) {
+      lastpage = totalpage.slice(totalpage.indexOf('page=') + 5)
+    }
+    else {
+      lastpage = '1'
+    }
+
+    var novels = {
+      url: URL + page,
+      page: parseInt(page),
+      data: data,
+      totalpage: parseInt(lastpage)
+    };
+
+    res.send(JSON.stringify(novels));
+  });
+});
+
+module.exports = router;
